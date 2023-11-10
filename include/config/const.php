f774a68d198cbeb851e4903f704e2a32aafb4023
Yoshinori Hirose <yoshinorihirosetraining@gmail.com>
2023-11-10 13:20:15 +0900

    DB接続先をレンタルサーバーに変更

--- include/config/const.php
+++ include/config/const.php
@@ -1,5 +1,11 @@
 <?php
 
-const DB_DSN = 'mysql:host=localhost;dbname=bcdhm_114';
-const DB_LOGIN_USER = 'root';
-const DB_PASSWORD = 'root';
+/* エックスサーバー */
+const DB_DSN = 'mysql:host=localhost;dbname=xb513874_ohncj';
+const DB_LOGIN_USER = 'xb513874_e021p';
+const DB_PASSWORD = 'sk5vn0q8pw';
+
+/* MAMP */
+//const DB_DSN = 'mysql:host=localhost;dbname=bcdhm_114';
+//const DB_LOGIN_USER = 'root';
+//const DB_PASSWORD = 'root';
