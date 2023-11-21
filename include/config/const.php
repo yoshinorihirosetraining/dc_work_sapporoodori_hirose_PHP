4d02d692f27553ccceb6830ca2ea5844824e05c8
Yoshinori Hirose <yoshinorihirosetraining@gmail.com>
2023-11-21 11:55:44 +0900

    サーバーをエックスサーバーに変更

--- include/config/const.php
+++ include/config/const.php
@@ -1,11 +1,11 @@
 <?php
 
 /* エックスサーバー */
-// const DB_DSN = 'mysql:host=localhost;dbname=xb513874_ohncj';
-// const DB_LOGIN_USER = 'xb513874_e021p';
-// const DB_PASSWORD = 'sk5vn0q8pw';
+const DB_DSN = 'mysql:host=localhost;dbname=xb513874_ohncj';
+const DB_LOGIN_USER = 'xb513874_e021p';
+const DB_PASSWORD = 'sk5vn0q8pw';
 
 /* MAMP */
-const DB_DSN = 'mysql:host=localhost;dbname=bcdhm_114';
-const DB_LOGIN_USER = 'root';
-const DB_PASSWORD = 'root';
+// const DB_DSN = 'mysql:host=localhost;dbname=bcdhm_114';
+// const DB_LOGIN_USER = 'root';
+// const DB_PASSWORD = 'root';
